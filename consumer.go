3124142e1ac5ba072830ef43a29a5b106f1559c0
seagullbird <seagullbird823@gmail.com>
2018-02-11 17:55:57 +0800

    added path existence check

--- consumer.go
+++ consumer.go
@@ -22,10 +22,17 @@ func consumeEvents(newsiteChannel chan NewSiteEvent) {
 
 func generateNewSite(event NewSiteEvent) {
 	log.Printf("Received newsite event: %s", event)
-	cmd := exec.Command("hugo", "new", "site", filepath.Join(sitesDir, event.Email, event.SiteName))
+	sitepath := filepath.Join(sitesDir, event.Email, event.SiteName)
+
+	if _, err := os.Stat(sitepath); err == nil || !os.IsNotExist(err) {
+		log.Printf("Path %s already exists: %v", sitepath, err)
+		return
+	}
+	cmd := exec.Command("hugo", "new", "site", sitepath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		failOnError(err, "Failed to generate new site")
 	}
 }
+
