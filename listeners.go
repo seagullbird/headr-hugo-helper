fc2fe0d9596a9c0dee7b8f827fe6c5f429012509
seagullbird <seagullbird823@gmail.com>
2018-02-19 19:27:35 +0800

    adapted to headr-common

--- listeners.go
+++ listeners.go
@@ -5,30 +5,34 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"github.com/seagullbird/headr-common/mq_helper"
+	"github.com/seagullbird/headr-common/mq/receive"
 	"github.com/streadway/amqp"
 	"encoding/json"
 	"fmt"
+	"github.com/seagullbird/headr-common/mq"
 )
 
-func generateNewSite(delivery amqp.Delivery) {
-	var event mq_helper.NewSiteEvent
-	err := json.Unmarshal(delivery.Body, &event)
-	if err != nil {
-		log.Println("Failed to unmarshal event:", err, "raw message:", delivery.Body)
-		return
-	}
 
-	log.Println("Received newsite event:", event)
-	sitepath := filepath.Join(sitesDir, event.Email, event.SiteName)
+func makegenerateNewSiteListener(logger log.Logger) receive.Listener {
+	return func(delivery amqp.Delivery) {
+		var event mq.NewSiteEvent
+		err := json.Unmarshal(delivery.Body, &event)
+		if err != nil {
+			log.Println("Failed to unmarshal event:", err, "raw message:", delivery.Body)
+			return
+		}
 
-	if _, err := os.Stat(sitepath); err == nil || !os.IsNotExist(err) {
-		log.Println(fmt.Sprintf("Path %s already exists.", sitepath))
-		return
-	}
-	cmd := exec.Command("hugo", "new", "site", sitepath)
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		log.Println( "Failed to generate new site", err)
+		log.Println("Received newsite event:", event)
+		sitepath := filepath.Join(sitesDir, event.Email, event.SiteName)
+
+		if _, err := os.Stat(sitepath); err == nil || !os.IsNotExist(err) {
+			log.Println(fmt.Sprintf("Path %s already exists.", sitepath))
+			return
+		}
+		cmd := exec.Command("hugo", "new", "site", sitepath)
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			log.Println( "Failed to generate new site", err)
+		}
 	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	"github.com/seagullbird/headr-common/mq_helper"
+	"github.com/seagullbird/headr-common/mq"
+	"github.com/seagullbird/headr-common/mq/receive"
+	"github.com/go-kit/kit/log"
+	"os"
 )
 
 func main() {
-	receiver := mq_helper.NewReceiver()
-	receiver.RegisterListener(newsiteQueueName, generateNewSite)
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	var (
+		servername = mq.MQSERVERNAME
+		username   = mq.MQUSERNAME
+		passwd     = mq.MQSERVERPWD
+	)
+
+	conn, err := mq.MakeConn(servername, username, passwd)
+	if err != nil {
+		logger.Log("error_desc", "mq.MakeConn failed", "error", err)
+		return
+	}
+	receiver, err := receive.NewReceiver(conn, logger)
+	if err != nil {
+		logger.Log("error_desc", "receive.NewReceiver failed", "error", err)
+		return
+	}
+	receiver.RegisterListener(newsiteQueueName, makegenerateNewSiteListener(logger))
 
 	forever := make(chan bool)
 	<-forever
